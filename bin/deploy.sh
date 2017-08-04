b7d6161c8b2f8e46efb9ecda0e083088d23ad81e
cerico <garethrobertlee@gmail.com>
2017-08-04 06:37:00 +0100

    run in detached mode

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 eval $(docker-machine env marsden)
-docker-compose -f docker-compose.prod.yml build && docker-compose -f docker-compose.prod.yml up
+docker-compose -f docker-compose.prod.yml build && docker-compose -f docker-compose.prod.yml up -d
 eval $(docker-machine env -u)
