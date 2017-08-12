e569fa7f25ad4ae2822d1576c4dbf24fdc5c4a70
cerico <garethrobertlee@gmail.com>
2017-08-12 10:12:42 +0100

    restart always

--- bin/digital_ocean/deploy.sh
+++ bin/digital_ocean/deploy.sh
@@ -1,5 +1,4 @@
 source config/mars.env
-
 eval $(docker-machine env $digital_ocean_machine)
 docker-compose -f docker-compose.prod.yml build && docker-compose -f docker-compose.prod.yml up -d
 eval $(docker-machine env -u)
